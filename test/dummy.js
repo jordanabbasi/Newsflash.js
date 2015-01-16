32aba313165af5aed23e784d2717f68fbbeb7165
Raul Jordan <rauljordan@college.harvard.edu>
2015-01-16 17:40:33 -0600

    added skeleton of project

---
+++ test/dummy.js
@@ -0,0 +1,12 @@
+var test = require('tape');
+var newsflash = require('../');
+
+/**
+ * Tests the making of a niche object without setting the value  
+ */
+test('dummy', function (t) {
+	
+    t.equals(1, 1);
+    t.end();
+   
+});
